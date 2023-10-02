33a354d7d9be94f07727c86f8c34cdbded8b72a1
giuseppeandreacosta <giuseppeandreac@gmail.com>
2023-10-02 19:06:31 +0200

    commit details.js

--- details.js
+++ details.js
@@ -1,30 +1,38 @@
 const params = new URLSearchParams(window.location.search);
 const asin = params.get("asin");
 
-
-
 fetch(`https://striveschool-api.herokuapp.com/books/?id=${asin}`)
   .then(response => response.json())
-  .then(bookAsinShow) 
-    // Qui puoi generare l'HTML per visualizzare i dettagli del libro
-  
+  .then(data => {
+    bookAsinShow(data);
+    addEventListeners(data);
+  });
 
-
-    // Aggiungi un gestore di eventi per ogni libro
-    // booksData.forEach((book) => {
-    //     document.getElementById(book.asin).addEventListener('click', () => {
-    //         console.log(books[book.asin]);
-    //     });
-    // });
-
-console.log(fetch)
-
-const bookAsin = document.querySelector(".cards-container")
+const bookAsin = document.querySelector(".cards-container");
 
 function bookAsinShow(data) {
-        console.log(data.book)
-    bookAsin.innerHTML = data.book
+  console.log(data); // Stampa l'intero oggetto per vedere la sua struttura
+  let html = '';
+  data.forEach((book) => {
+    html += `
+      <div id="${book.asin}" class="card">
+        <img src="${book.img}" alt="book image">
+            <div class="details" id="details-${book.asin}">
+                <span>Title: ${book.title}</span>
+                <span>Price: € ${book.price}</span>
+                <span>ASIN: ${book.asin}</span>
+            </div>
+      </div>
+    `;
+  });
+  bookAsin.innerHTML = html;
 }
 
-
-
+function addEventListeners(booksData) {
+  // Ora booksData dovrebbe essere un array di oggetti libro
+  booksData.forEach((book) => {
+    document.getElementById(`details-${book.asin}`).addEventListener('click', () => {
+      console.log(book); // Stampa l'intero oggetto libro quando si fa clic sulla sezione "details"
+    });
+  });
+}
